48daf798145674d7129139be2864612b75f84e62
Julien Isorce <j.isorce@samsung.com>
2016-06-07 13:08:51 +0100

    WIP: use DMABuf caps feature
    Compared to other branches, here this is just to
    always use the DMABuf caps feature for testing.
    
    Signed-off-by: Julien Isorce <j.isorce@samsung.com>

--- gst/vaapi/gstvaapipostproc.c
+++ gst/vaapi/gstvaapipostproc.c
@@ -64,6 +64,7 @@ static const char gst_vaapipostproc_sink_caps_str[] =
 static const char gst_vaapipostproc_src_caps_str[] =
   GST_VAAPI_MAKE_SURFACE_CAPS ", "
   GST_CAPS_INTERLACED_FALSE "; "
+  GST_VAAPI_MAKE_DMABUF_CAPS "; "
 #if (USE_GLX || USE_EGL)
   GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS "; "
 #endif
